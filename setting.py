da5ca296760a4fb5b05b872d2c02f0c6e1110e53
Graf_35 <Graf3456718@yandex.ru>
2023-07-26 20:04:31 +0300

    Оптимизирован файл main_window.py,  Добавлено конфигурация и настройка

---
+++ setting.py
@@ -0,0 +1,31 @@
+from PyQt5 import QtWidgets
+from PyQt5 import  uic
+import Scripts
+
+
+#Определяем имяи путь до файлас формой окна.
+ui=uic.loadUiType("interface/setting.ui")[0]
+
+class Setting(QtWidgets.QMainWindow, ui):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.apply_button.clicked.connect(self.apply_button_clicked)
+        self.config = Scripts.filereader("config.config")
+        self.developer_mode.setText("Режим разработчика")
+        self.developer_mode_value.setText(self.config["developer_mode"])
+        self.rabbimq_main_server.setText("Адрес сервера RabbitMQ")
+        self.rabbimq_main_server_value.setText(self.config["rabbimq_main_server"])
+
+
+    def apply_button_clicked(self):
+        lines=''
+        self.config["developer_mode"]=self.developer_mode_value.text()
+        self.config["rabbimq_main_server"]=self.rabbimq_main_server_value.text()
+        for key in self.config:
+           lines+=str (key)+";"+str(self.config[key]+"\n")
+        with open("config.config", "w") as file:
+            for line in lines:
+                if line != "":
+                    file.write(line)
+
